a847fd52d9fd840e649c3c89344fb6e595fa9996
SebasIBR <negraoo477@gmail.com>
2023-03-14 11:12:22 -0500

    Avance del frontEnd

--- front/src/components/Query.js
+++ front/src/components/Query.js
@@ -1,17 +1,29 @@
 import React from "react";
 import "../assets/css/Query.css"
 
-function Query(){
-    return(
+function Query() {
+    return (
         <main className="mainQuery">
-            <section>
+            <section className="queryForm">
                 {/* Seccion de consulta de imagenes */}
-                
-
+                <form className="formQuery">
+                    <label className="labelFormQuery">Fecha incial</label>
+                    <input className="inputFormQuery" type={"date"}></input>
+                    <label className="labelFormQuery">Fecha Final</label>
+                    <input className="inputFormQuery" type={"date"}></input>
+                    <label className="labelFormQuery">Seleccione hora a Consultar</label>
+                    <select className="selectFormQuery">
+                        <option className="optionFormQuery">Cargando...</option>
+                    </select>
+                    <button className="buttonQuery">Consultar</button>
+                </form>
             </section>
-            <section>
-                {/* Seccion de listado de imagenes */}
-
+            <section className="queryList">
+                {/* Listado de imagenes que se han subido por dia */}
+                <ul className="list">
+                    <li>Cargando...</li>
+                    <li>Cargando...2</li>
+                </ul>
 
             </section>
         </main>
